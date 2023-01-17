e643ced5ca1067bdc1164b065517c629b75749f6
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-01-17 13:26:00 -0300

    tipos de funcoes

---
+++ 3-FuncoesAvancadas/1-tiposFuncoes.js
@@ -0,0 +1,17 @@
+// funcoes 
+
+function ola1(text) {
+    console.log(text); 
+}
+
+let ola2 = function (text){
+    console.log(text);
+}
+
+let ola3 = (text)=>{
+    console.log(text); 
+}
+
+ola1("1");
+ola2("2");
+ola3("3");
