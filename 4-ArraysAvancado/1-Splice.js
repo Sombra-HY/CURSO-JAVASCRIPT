86b62a79d2a97cd04f956f886367ba1d2c2ebc88
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-01-19 22:31:18 -0300

    splice

---
+++ 4-ArraysAvancado/1-Splice.js
@@ -0,0 +1,8 @@
+// splice (inidice, delete , addelemento1, addelemento2, ...){
+//      return [deletados]   
+//}
+
+let nomes = ['lucas','jose','maria','ronaldo','gabi']; 
+ let deletados= nomes.splice(0,4,'junior');
+
+console.log(nomes,deletados);
