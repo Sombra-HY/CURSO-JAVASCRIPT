c3dbb5094710c090f77ca0ab2d334ee4adf7b3bb
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-02-08 13:00:39 -0300

    Nodemon + Express

---
+++ 9-Node/6-express_nodemon/server.js
@@ -0,0 +1,33 @@
+// NODEMON 
+
+// o servidor e atualizado periodicamente ao atualizar a pagina
+// sem ser necessario desligar e ligar o servidor...
+
+// é uma dependencia de desenvolvimento, pois ela so e necessaria no nosso computador para o desenvolvimento 
+// do app// site...
+
+// é possivel rodar atraves do npx nodemon server.js <-- arquivo
+
+const express = require('./node_modules/express');
+const app = express();
+
+app.get('/',(requisao, resposta) => {
+    resposta.send('Opa meu bom !');
+})
+
+app.get('/mandaaee',(req,resp)=>{
+    resp.send(`
+    <form action="/mandaaee" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>Enviar</button>
+    </form>
+    `);
+})
+
+app.post('/mandaaee',(req,resp)=>{
+    resp.send('bom dia!!');
+})
+
+app.listen(3000,()=>{
+    console.log('Acessar: http://localhost:3000/ rodando na porta 3000!!');
+});
