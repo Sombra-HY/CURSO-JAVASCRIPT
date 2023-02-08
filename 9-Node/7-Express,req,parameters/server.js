077c35d2bcabf5777dac64df26726ea10c32aac9
Lucas sombra do nascimento <l.sombra.nascimento@gmail.com>
2023-02-08 16:04:39 -0300

    Tipos de Requerimentos

--- 9-Node/7-Express,req,parameters/server.js
+++ 9-Node/7-Express,req,parameters/server.js
@@ -1,3 +1,13 @@
+// QUERY STRING
+// valores pelo url GET
+
+// POST
+// para pegar os valores de um form é necessario: (req.body.'exemplo') 
+// app.use(
+//     express.urlencoded({extended:true})
+// )
+
+
 const express = require('./node_modules/express');
 const app = express();
 
@@ -5,17 +15,33 @@ const app = express();
 
 app.get('/',(req,resp)=>{
     resp.send(`
-    <form action="/mandaaee" method="POST">
+    <form action="/" method="POST">
     Nome: <input type="text" name="nome">
     <button>Enviar</button>
     </form>
     `);
-    console.log('aatt');
+})
+// sem coolocar o '?' fica opcional receber ou nao os valores--> podendo acessar o /teste 
+app.get('/teste/:Idusuarios?/:valores?',(req,resp)=>{
+    console.log(req.params);
+    console.log(req.query); // <-- imprime query strings por exemplo na 
+                            //url https:.../teste/?nome=junior&sobrenome=orivaldo
+    resp.send(req.query.nome);
+})
+// POST
+
+// para pegar os dados enviados pelo metodo post é necessario do codigo abaixo:
+app.use(
+    express.urlencoded({extended:true})
+);
+
+app.post('/',(req,resp) => {
+    console.log(req.body);
+    resp.send(`o que vc me enviou foi ${req.body.nome}`); // nome <-- é o name='name' no form
+
 })
 
-app.post('/mandaaee',(req,resp)=>{
-    resp.send('bom dia!!');
-})
+
 
 app.listen(3000,()=>{
     console.log('Acessar: http://localhost:3000/ rodando na porta 3000!!');
